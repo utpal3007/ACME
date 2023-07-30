4df7854bbaac916193afa3667626c76556d697ce
Utpal Kumar <utpal3007@gmail.com>
2023-07-31 00:20:39 +0200

    add

--- acme_iac/main.tf
+++ acme_iac/main.tf
@@ -2,36 +2,59 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "acme_rg" {
-  name     = "acme_rg"
-  location = "West Europe"
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resource-group"
+  location = "East US"  # Change this to your desired Azure region
 }
 
-resource "azurerm_app_service_plan" "acme_app_plan" {
-  name                = "acme-service-plan"
-  location            = azurerm_resource_group.app_rg.location
-  resource_group_name = azurerm_resource_group.app_rg.name
-  kind                = "Linux"
-  reserved            = true
+# Create an Azure App Service Plan for your web app
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-app-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"  # For Linux web app plan
+  reserved            = true    # Set this to true for Linux plans
+
 
   sku {
-    tier = "Basic"
-    size = "B1"
+    tier = "Basic"  # Change SKU as needed
+    size = "B1"     # Change SKU size as needed
   }
 }
 
-resource "azurerm_app_service" "acme_app_service" {
-  name                = "acme_app_service"
-  location            = azurerm_resource_group.app_rg.location
-  resource_group_name = azurerm_resource_group.app_rg.name
-  app_service_plan_id = azurerm_app_service_plan.acme_app_plan.id
+# Create a web app (main Azure App Service) and enable Local Git deployment
+resource "azurerm_app_service" "example" {
+  name                = "acme-app-svc"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
 
   site_config {
     always_on = true
-    linux_fx_version = "DOCKER|<your_docker_hub_username>/my-app-container:latest"
+
+    scm_type = "LocalGit"
   }
 
-  app_settings = {
-    WEBSITES_PORT = "8080"
+  identity {
+    type = "SystemAssigned"
   }
+}
+
+# Create a deployment slot
+resource "azurerm_app_service_slot" "example" {
+  name                = "staging"  # Change the slot name as needed
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_name    = azurerm_app_service.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+}
+
+# Output the URLs of the main app and the deployment slot
+output "main_app_url" {
+  value = azurerm_app_service.example.default_site_hostname
+}
+
+output "slot_url" {
+  value = azurerm_app_service_slot.example.default_site_hostname
 }
